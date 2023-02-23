0d93cd8583e7bc63a354c350a3a118f9020f9096
Oluwaseun <oluseun.ismaila@gmail.com>
2023-02-23 06:07:24 -0600

    added email js

---
+++ email.js
@@ -0,0 +1,11 @@
+function isEmail(emailAdress){
+    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if (emailAdress.match(regex)) 
+    return true; 
+
+   else 
+    return false; 
+}
+
+
